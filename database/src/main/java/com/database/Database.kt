4f394f468f5aa5c6ffc58250778d26ca6260573f
Rodrigo Pedro <rodrigopedro@gmail.com>
2023-01-25 12:08:21 -0300

    Creating database writting methods for services and products

--- database/src/main/java/com/database/Database.kt
+++ database/src/main/java/com/database/Database.kt
@@ -1,11 +1,19 @@
 package com.database
 
 import com.database.Constants.ATTRIBUTE_FOOD_ID_LIST
+import com.database.Constants.ATTRIBUTE_PRODUCT_ID_LIST
+import com.database.Constants.ATTRIBUTE_SERVICE_ID_LIST
 import com.database.Constants.FOOD_COLLECTION
+import com.database.Constants.PRODUCT_COLLECTION
+import com.database.Constants.SERVICE_COLLECTION
 import com.database.Constants.TEST_FOOD_COLLECTION
+import com.database.Constants.TEST_PRODUCT_COLLECTION
+import com.database.Constants.TEST_SERVICE_COLLECTION
 import com.database.Constants.TEST_USER_COLLECTION
 import com.database.Constants.USER_COLLECTION
 import com.domain.model.Food
+import com.domain.model.Product
+import com.domain.model.Service
 import com.domain.model.UserInfo
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.ktx.firestore
@@ -61,11 +69,65 @@ object Database {
         }
     }
 
+    fun saveProduct(
+        newProduct: Product,
+        onSuccess: () -> Unit = {},
+        onFailure: () -> Unit = {}
+    ) {
+        val newProductDocumentReference = getProductCollection().document()
+        val userDocumentReference = getUserCollection().document(LoggedUser.info.email)
+        val newProductId = newProductDocumentReference.id
+        val newUserProductIdList = LoggedUser.info.productIdList + newProductId
+        newProductDocumentReference.set(newProduct).addOnSuccessListener {
+            userDocumentReference.update(ATTRIBUTE_PRODUCT_ID_LIST, newUserProductIdList)
+                .addOnSuccessListener {
+                    LoggedUser.info.productIdList = newUserProductIdList
+                    onSuccess.invoke()
+                }
+                .addOnFailureListener {
+                    onFailure.invoke()
+                }
+        }.addOnFailureListener {
+            onFailure.invoke()
+        }
+    }
+
+    fun saveService(
+        newService: Service,
+        onSuccess: () -> Unit = {},
+        onFailure: () -> Unit = {}
+    ) {
+        val newServiceDocumentReference = getServiceCollection().document()
+        val userDocumentReference = getUserCollection().document(LoggedUser.info.email)
+        val newServiceId = newServiceDocumentReference.id
+        val newUserServiceIdList = LoggedUser.info.serviceIdList + newServiceId
+        newServiceDocumentReference.set(newService).addOnSuccessListener {
+            userDocumentReference.update(ATTRIBUTE_SERVICE_ID_LIST, newUserServiceIdList)
+                .addOnSuccessListener {
+                    LoggedUser.info.serviceIdList = newUserServiceIdList
+                    onSuccess.invoke()
+                }
+                .addOnFailureListener {
+                    onFailure.invoke()
+                }
+        }.addOnFailureListener {
+            onFailure.invoke()
+        }
+    }
+
     private fun getUserCollection(): CollectionReference = getFirestoreInstance().collection(
         if (BuildConfig.DEBUG) TEST_USER_COLLECTION else USER_COLLECTION
     )
 
     private fun getFoodCollection(): CollectionReference = getFirestoreInstance().collection(
         if (BuildConfig.DEBUG) TEST_FOOD_COLLECTION else FOOD_COLLECTION
+    )
+
+    private fun getServiceCollection(): CollectionReference = getFirestoreInstance().collection(
+        if (BuildConfig.DEBUG) TEST_SERVICE_COLLECTION else SERVICE_COLLECTION
+    )
+
+    private fun getProductCollection(): CollectionReference = getFirestoreInstance().collection(
+        if (BuildConfig.DEBUG) TEST_PRODUCT_COLLECTION else PRODUCT_COLLECTION
     )
 }
--- domain/src/main/java/com/domain/model/UserInfo.kt
+++ domain/src/main/java/com/domain/model/UserInfo.kt
@@ -6,5 +6,7 @@ data class UserInfo(
     val wpp: String = "",
     val block: String = "",
     val apartment: String = "",
-    var foodIdList: List<String> = listOf()
+    var foodIdList: List<String> = listOf(),
+    var serviceIdList: List<String> = listOf(),
+    var productIdList: List<String> = listOf()
 )
